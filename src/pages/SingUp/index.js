544a0e37721674a31ee6ffe95afaeeb5396312e8
Pedro Nascimento <nennogabriel@gmail.com>
2019-09-14 12:16:23 -0300

    09a09 Estilização da autenticação

--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
 export default function SingUp() {
-  return <h1>Register</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input type="name" placeholder="Nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/">Já tenho Login</Link>
+      </form>
+    </>
+  );
 }
