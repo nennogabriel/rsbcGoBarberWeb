44d6310f1fe074da245d012fb0a3e2df97cd51b2
Pedro Nascimento <nennogabriel@gmail.com>
2019-09-14 12:38:35 -0300

    09a11 Validações

--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
-// import { Container } from './styles';
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O email é obrigatório'),
+  password: Yup.string()
+    .min(6, 'No mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
 
 export default function SingUp() {
   function handleSubmit(data) {
@@ -14,7 +23,7 @@ export default function SingUp() {
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" type="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input
