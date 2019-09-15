fae6c7bb183ade923297e17d0a1761fde6f850db
Pedro Nascimento <nennogabriel@gmail.com>
2019-09-15 12:37:30 -0300

    09a29 Navegando entre Dias

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { format, subDays, addDays } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
 import { Container, Time } from './styles';
 
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
+
+  function handlePrevDate() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDate() {
+    setDate(addDays(date, 1));
+  }
+
   return (
     <Container>
       <header>
-        <button type="button">
+        <button type="button" onClick={handlePrevDate}>
           <MdChevronLeft size={36} color="#fff" />
         </button>
-        <strong>31 de Maio</strong>
-        <button type="button">
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleNextDate}>
           <MdChevronRight size={36} color="#fff" />
         </button>
       </header>
