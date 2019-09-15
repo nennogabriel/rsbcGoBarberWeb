e6177562bddb58de9a906a41039630fcc5143181
Pedro Nascimento <nennogabriel@gmail.com>
2019-09-15 12:03:40 -0300

    09a27 Logout da aplicação

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
-import { Container } from './styles';
+import { signOut } from '~/store/modules/auth/actions';
 import { updateProfileRequest } from '~/store/modules/user/action';
 
 import AvatarInput from './AvatarInput';
+
+import { Container } from './styles';
 
 export default function Profile() {
   const dispatch = useDispatch();
@@ -13,6 +15,10 @@ export default function Profile() {
 
   function handleSubmit(data) {
     dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
   }
   return (
     <Container>
@@ -38,7 +44,9 @@ export default function Profile() {
 
         <button type="submit">Atualizar perfil</button>
       </Form>
-      <button type="button">Sair do GoBarber</button>
+      <button type="button" onClick={handleSignOut}>
+        Sair do GoBarber
+      </button>
     </Container>
   );
 }
